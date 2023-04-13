ffc18bcfbbfba3de9011ade60ff9de15ef2981e4
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-13 18:16:17 -0300

    :sparkles: feat: criando test para a rota dados - método get

--- back-end/tests/integration/data.test.js
+++ back-end/tests/integration/data.test.js
@@ -1,16 +1,30 @@
 const chai = require('chai');
+const sinon = require('sinon');
+const fs = require('fs');
 const chaiHttp = require('chai-http');
 const app = require('../../src/app');
+const dataMock = require('../mocks/dataMock.json')
 
 const { expect } = chai;
 
 chai.use(chaiHttp);
 
 describe('Usando o método GET em /dados', () => {
+    beforeEach(function () {
+        sinon.stub(fs.promises, 'readFile')
+          .resolves(dataMock);
+      });
+    
+    
+      afterEach(function () {
+        sinon.restore();
+      });
+
     it('Retorna a lista informações completa!', async function () {
         const response = await chai
             .request(app) 
             .get('/dados');
         expect(response.status).to.be.equal(200);
+        expect(response.body[0]).to.deep.equal(dataMock[0]);
     });
 });
