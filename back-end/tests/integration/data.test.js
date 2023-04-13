ff6eb6f8e6e0af19a2a632b8133272d71fb0897a
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-13 18:57:46 -0300

    :bug: fix : corrigindo erros

--- back-end/tests/integration/data.test.js
+++ back-end/tests/integration/data.test.js
@@ -3,24 +3,23 @@ const sinon = require('sinon');
 const fs = require('fs');
 const chaiHttp = require('chai-http');
 const app = require('../../src/app');
-const dataMock = require('../mocks/dataMock.json')
+const dataMock = require('../mocks/dataMock.json');
 
 const { expect } = chai;
 
 chai.use(chaiHttp);
 
 describe('Usando o método GET em /dados', () => {
-    beforeEach(function () {
+    beforeEach(() => {
         sinon.stub(fs.promises, 'readFile')
           .resolves(dataMock);
       });
     
-    
-      afterEach(function () {
+      afterEach(() => {
         sinon.restore();
       });
 
-    it('Retorna a lista informações completa!', async function () {
+    it('Retorna a lista informações completa!', async () => {
         const response = await chai
             .request(app) 
             .get('/dados');
