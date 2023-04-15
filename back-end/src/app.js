6f04c973bf39fc831e8305b91938a6923063adfa
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-14 21:34:45 -0300

    :wrench: test

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,6 +4,10 @@ const DataRoute = require('./routes/DataRoute');
 const app = express();
 
 app.use(express.json());
+app.get('/', (req, res) => {
+    return res.json('hello world');
+});
+
 app.use(DataRoute);
 
 module.exports = app;
