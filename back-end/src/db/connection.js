4c11e1a483708b92be37a939745464b2d3050bf5
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-14 21:17:19 -0300

    :wrench: configurando arquivo para deploy da api

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
-const { MYSQL_USER, MYSQL_ROOT_PASSWORD, MYSQL_HOST, MYSQL_PORT } = process.env;
+const { MYSQL_USER, MYSQL_ROOT_PASSWORD, MYSQL_PORT } = process.env;
 
 const connection = mysql.createPool({
-    host: MYSQL_HOST,
     port: MYSQL_PORT,
     user: MYSQL_USER,
     password: MYSQL_ROOT_PASSWORD,
