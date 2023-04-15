358ba77deb64c87d47c3ce93f15b033239320ff6
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-15 00:31:47 -0300

    :wrench: test

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const DataRoute = require('./routes/DataRoute');
+const dataMock = require('../tests/mocks/dataMock.json');
 
 const app = express();
 
 app.use(express.json());
-app.get('/', (req, res) => res.json('hello world'));
+app.get('/', (req, res) => res.json(dataMock));
 
 app.use(DataRoute);
 
