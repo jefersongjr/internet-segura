c711a80cea7e6fbdcef8564e13560c1c5d8bbf97
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-17 18:12:07 -0300

    :wrench: test

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
-const { MYSQL_USER, MYSQL_ROOT_PASSWORD, MYSQL_HOST, MYSQL_PORT } = process.env;
+const { MYSQL_USER, MYSQL_ROOT_PASSWORD, MYSQL_HOST, MYSQL_PORT , MYSQL_DB} = process.env;
+
+const PLANETSCALE_DB_HOST = MYSQL_HOST;
+const PLANETSCALE_DB = MYSQL_DB;
+const PLANETSCALE_DB_PASSWORD = MYSQL_ROOT_PASSWORD;
+const PLANETSCALE_DB_USERNAME = MYSQL_USER;
+
 
 const connection = mysql.createPool({
-    host: MYSQL_HOST,
+    host: PLANETSCALE_DB_HOST,
     port: MYSQL_PORT,
-    user: MYSQL_USER,
-    password: MYSQL_ROOT_PASSWORD,
-    database: 'informacoes',
+    user: PLANETSCALE_DB_USERNAME,
+    password: PLANETSCALE_DB_PASSWORD,
+    database: PLANETSCALE_DB,
     charset: 'utf8mb4',
 });
 
