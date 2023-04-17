40f825077f433e619e3666b169ce0ddf3437f24f
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-17 18:17:05 -0300

    :sparkles: iniciando axios

--- front-end/app/src/pages/Search.jsx
+++ front-end/app/src/pages/Search.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
 function Search() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get('https://internet-segura-api.vercel.app/dados')
+      .then((res) => {
+      setData(res)
+    } );
+  },[])
   return (
     <div className="Search">
        <button>Criança</button>
+       {console.log(data)}
        <button>Adolescente</button>
     </div>
   )
