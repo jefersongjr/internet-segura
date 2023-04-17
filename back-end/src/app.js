6db15080399891f3d4b7b172e2379f5eb94b1cb2
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-17 19:24:05 -0300

    :bug: fix :  testando o deploy

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const DataRoute = require('./routes/DataRoute');
-const dataMock = require('../tests/mocks/dataMock.json');
+// const dataMock = require('../tests/mocks/dataMock.json');
 
 const app = express();
 
 app.use(express.json());
-app.get('/', (req, res) => res.json(dataMock));
 
 app.use(DataRoute);
 
