3ed3ae64c629d844b5291af3353af040c9a4df46
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-18 12:01:27 -0300

    :hammer: refactor : refatorando a api para usar o banco de dados PostgreeSql

--- back-end/src/models/DataModel.js
+++ back-end/src/models/DataModel.js
@@ -1,11 +1,11 @@
-const connection = require('../db/connection');
+const pool = require('../db/connection');
 
 const findData = async () => {
-  const [result] = await connection.execute(
-    `SELECT dados_id, img_url, titulo, conteudo, faixa_etaria, tipo_info
-    FROM dados
-    JOIN faixa_etaria ON dados.faixa_etaria_id = faixa_etaria.faixa_etaria_id
-    JOIN tipo_info ON dados.tipo_id = tipo_info.tipo_id`,
+  const result = await pool.query(
+    ` SELECT dados_id, img_url, titulo, conteudo, faixa_etaria, tipo_info
+    FROM informacoes.dados as dados
+     JOIN informacoes.faixa_etaria as faixa_etaria ON dados.faixa_etaria_id = faixa_etaria.faixa_etaria_id
+    JOIN informacoes.tipo_info as tipo_info ON dados.tipo_id = tipo_info.tipo_id`,
   );
   return result;
 };
