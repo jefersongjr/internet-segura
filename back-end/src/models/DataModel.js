64ae4e2001ede4919c2c1a3495a3b1297653aee1
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-18 15:54:19 -0300

    :bug: fix : deploy corringindo model

--- back-end/src/models/DataModel.js
+++ back-end/src/models/DataModel.js
@@ -1,13 +1,13 @@
-const pool = require('../db/connection').default;
+const { pool } = require('../db/connection');
 
 const findData = async () => {
   const result = await pool.query(
-    ` SELECT dados_id, img_url, titulo, conteudo, faixa_etaria, tipo_info
-    FROM informacoes.dados as dados
+    `SELECT dados_id, img_url, titulo, conteudo, faixa_etaria, tipo_info
+     FROM informacoes.dados as dados
      JOIN informacoes.faixa_etaria as faixa_etaria ON dados.faixa_etaria_id = faixa_etaria.faixa_etaria_id
-    JOIN informacoes.tipo_info as tipo_info ON dados.tipo_id = tipo_info.tipo_id`,
+     JOIN informacoes.tipo_info as tipo_info ON dados.tipo_id = tipo_info.tipo_id`,
   );
-  return result;
+  return result.rows;
 };
 
 module.exports = {
