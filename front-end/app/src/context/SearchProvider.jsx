f6559c209b3444e0aad1d1683d345dd1a1ebb8c1
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-19 16:51:33 -0300

    :recycle: refactor: refatorando código , fazendo requisição da api no provider

--- front-end/app/src/context/SearchProvider.jsx
+++ front-end/app/src/context/SearchProvider.jsx
@@ -1,24 +1,34 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import SearchContext from './SearchContext';
+import axios from 'axios';
 
-function CartProvider({ children }) {
-    const [orders, setOrders] = useState([]);
-    const [total, setTotal] = useState();
+function SearchProvider({ children }) {
+    const [data, setData] = useState([]);
+    const [search, setSearch] = useState([]);
+  
+    useEffect(() => {
+      axios.get('https://internet-segura-api.vercel.app/')
+        .then((response) => {
+          setData(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }, []);
 
     const contextSearch = useMemo(() => ({
-      orders,
-      total,
-      setOrders,
-      setTotal,
-    }), []);
+      data,
+      search,
+      setSearch,
+    }), [data, search]);
     return (
       <SearchContext.Provider value={ contextSearch }>
         { children }
       </SearchContext.Provider>
     );
   }
-  export default CartProvider;
-  CartProvider.propTypes = {
+  export default SearchProvider;
+  SearchProvider.propTypes = {
     children: PropTypes.arrayOf(PropTypes.object),
   }.isRequired;
