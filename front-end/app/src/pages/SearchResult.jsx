22f0f89c10fa66cffc8f8b2a4c6930b915d2f6b8
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-19 17:14:02 -0300

    :sparkles: feat: renderizando os riscos na tela

--- front-end/app/src/pages/SearchResult.jsx
+++ front-end/app/src/pages/SearchResult.jsx
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react';
 import SearchContext from '../context/SearchContext';
+import RiskCard from '../components/RiskCard';
 
 function SearchResult() {
   const { search } = useContext(SearchContext);
 
   return (
     <div className="SearchResult"> 
-      {search.map((text) => <p>{text.tipo_info}</p>)}
+    <section className='riskContainer'>
+      {search.filter((type) => type.tipo_info === 'riscos')
+        .map((risk) => RiskCard(risk.titulo, risk.conteudo)
+      )}
+    </section>
     </div>
   )
 }
