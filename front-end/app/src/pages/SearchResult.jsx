ecfba7b98b42ba27ff207404f7125e4d46fb3579
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-19 16:52:25 -0300

    :sparkles: feat: recebendo dados do provider

--- front-end/app/src/pages/SearchResult.jsx
+++ front-end/app/src/pages/SearchResult.jsx
@@ -1,8 +1,12 @@
+import React, { useContext } from 'react';
+import SearchContext from '../context/SearchContext';
 
 function SearchResult() {
+  const { search } = useContext(SearchContext);
+
   return (
     <div className="SearchResult"> 
-      
+      {search.map((text) => <p>{text.tipo_info}</p>)}
     </div>
   )
 }
