e24c29afc8e8cfb9eb6dd95f31dc352ef38a7d35
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-19 15:41:09 -0300

    :sparkles: feat: filtrando pesquisa no clique do botão

--- front-end/app/src/pages/Search.jsx
+++ front-end/app/src/pages/Search.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 function Search() {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState([]);
+
 
   useEffect(() => {
     axios.get('https://internet-segura-api.vercel.app/')
@@ -13,12 +15,25 @@ function Search() {
         console.error(error);
       });
   }, []);
-  
 
+  const handleClick = ({ target }) => {
+    const { innerText } = target;
+    const filter = data.filter((type) => type.faixa_etaria === innerText.toLowerCase());
+    console.log(filter);
+    setSearch(filter);
+  }
+  
   return (
     <div className="Search">
-      <button>Criança</button>
-      <button>Adolescente</button>
+      <button
+        onClick={(e) => handleClick(e)}
+      >
+        Criança
+      </button>
+      <button
+        onClick={(e) => handleClick(e)}
+      >
+        Adolescente</button>
     </div>
   )
 }
