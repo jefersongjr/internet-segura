7268698d727094aab09171a69124fb848337ab9c
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-19 14:50:29 -0300

    :sparkles: feat: adicionando opções de cors

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const DataRoute = require('./routes/DataRoute');
 // const dataMock = require('../tests/mocks/dataMock.json');
 
 const app = express();
 
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+  }
+
+  
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 app.use(DataRoute);
 
