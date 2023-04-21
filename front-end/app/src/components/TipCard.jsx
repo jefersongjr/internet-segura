239f956c25b9e34cae34afdd4ebd66c7bf3cf921
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-21 01:16:08 -0300

    :sparkles: feat: adicionando classes

--- front-end/app/src/components/TipCard.jsx
+++ front-end/app/src/components/TipCard.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import '../styles/TipCard.css';
 
-function TipCard(img, description,  title, content) {
+function TipCard(img,  title, content) {
 return (
     <div className="TipCard">
-      <img src={`${ img }`}/>
+      <img 
+        src={` ${ img }` } 
+        alt={ `icon from icon8 ${ img }` }
+        className='imgCard'
+        />
       <h3 className='tipTitle'>{ title }</h3>
+      <div className='text'>
       <p className='tipContent'>{ content }</p>
+      </div>
     </div>
   );
 }
