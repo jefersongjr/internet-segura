48e55538d17b7b4bc20398eb7ce0bf01dbc74426
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-24 11:16:54 -0300

    :sparkles: feat: criando section do topo

--- front-end/app/src/pages/SearchResult.jsx
+++ front-end/app/src/pages/SearchResult.jsx
@@ -1,8 +1,8 @@
-import React, { useContext, useEffect} from 'react';
-import SearchContext from '../context/SearchContext';
+import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import SearchContext from '../context/SearchContext';
 import RiskCard from '../components/RiskCard';
-import '../styles/SearchResult.css'
+import '../styles/SearchResult.css';
 import TipCard from '../components/TipCard';
 
 function SearchResult() {
@@ -10,29 +10,43 @@ function SearchResult() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    search.length === 0 && navigate('/pesquisa');
+    const backToSelectSearch = () => {
+      const nav = search.length === 0 && navigate('/pesquisa');
+      return nav;
+    };
+    backToSelectSearch();
   });
 
   return (
     <div className="SearchResult">
-    <section className='riskContainer containers'>
-    <h2 className="riskContainerTitle title">Principais Ameaças</h2>
-      <div className='cardContainer'>
-      {search.filter((type) => type.tipo_info === 'riscos')
-        .map((risk) => RiskCard(risk.titulo, risk.conteudo)
-      )}
-      </div>
-    </section>
-    <section className='tipContainer containers'>
-    <h2 className="tipContainerTitle title">Como Previnir</h2>
-    <div className='cardTipContainer'>
-    {search.filter((type) => type.tipo_info === 'cuidados')
-        .map((tip) => TipCard(tip.img_url, tip.titulo, tip.conteudo)
-      )}
+      <section className="topContainer">
+        <div className="contentTopContainer">
+          <div className="textTopContainer">
+            <h2 className="title titleTop">Segurança para Adolescentes</h2>
+            <p>
+              O jovem, entre 10 e 17 anos, já possui um maior entendimento das coisas e,
+            </p>
+            <p>apesar disso, ainda é necessária a participação dos pais. </p>
+            <p>Nesse ponto, a educação deve ser mais forte do que a vigilância.</p>
+          </div>
+        </div>
+      </section>
+      <section className="riskContainer containers">
+        <h2 className="riskContainerTitle title">Principais Ameaças</h2>
+        <div className="cardContainer">
+          {search.filter((type) => type.tipo_info === 'riscos')
+            .map((risk) => RiskCard(risk.titulo, risk.conteudo))}
+        </div>
+      </section>
+      <section className="tipContainer containers">
+        <h2 className="tipContainerTitle title">Como Previnir</h2>
+        <div className="cardTipContainer">
+          {search.filter((type) => type.tipo_info === 'cuidados')
+            .map((tip) => TipCard(tip.img_url, tip.titulo, tip.conteudo))}
+        </div>
+      </section>
     </div>
-    </section>
-    </div>
-  )
+  );
 }
 
-export default SearchResult
+export default SearchResult;
