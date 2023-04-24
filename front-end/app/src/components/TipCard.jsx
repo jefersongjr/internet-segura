62260e16db5b543b1c27eff740288a0d35758274
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-24 16:14:04 -0300

    :rotating_light: lint: corrigindo lint:styles

--- front-end/app/src/components/TipCard.jsx
+++ front-end/app/src/components/TipCard.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../styles/TipCard.css';
 
-function TipCard(img,  title, content) {
-return (
+function TipCard(img, title, content) {
+  return (
     <div className="TipCard">
-      <img 
-        src={` ${ img }` } 
-        alt={ `icon from icon8 ${ img }` }
-        className='imgCard'
-        />
-      <h3 className='tipTitle'>{ title }</h3>
-      <div className='text'>
-      <p className='tipContent'>{ content }</p>
+      <img
+        src={ ` ${img}` }
+        alt={ `icon from icon8 ${img}` }
+        className="imgCard"
+      />
+      <h3 className="tipTitle">{ title }</h3>
+      <div className="text">
+        <p className="tipContent">{ content }</p>
       </div>
     </div>
   );
