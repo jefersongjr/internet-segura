4c2a942818a71268a022e4d905e99f9ba459b025
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-24 14:09:06 -0300

    :sparkles: feat: renderizando Header

--- front-end/app/src/components/Content.jsx
+++ front-end/app/src/components/Content.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Search from '../pages/Search';
 import SearchResult from '../pages/SearchResult';
+import Header from './Header';
 
 function Content() {
-
-return (
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/pesquisa" element={<Search />} />
-        <Route path="/resultado-pesquisa" element={<SearchResult />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
-      </Routes>
-    </Router>
-   </React.StrictMode>
+  return (
+    <React.StrictMode>
+      <Router>
+        <Header />
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/pesquisa" element={ <Search /> } />
+          <Route path="/resultado-pesquisa" element={ <SearchResult /> } />
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </Router>
+    </React.StrictMode>
   );
 }
 
