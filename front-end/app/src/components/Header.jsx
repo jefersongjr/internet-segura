66400a01ef001575a671836c7c44493df53c96a8
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-24 16:07:26 -0300

    :sparkles: feat: criando Menu hamburguer

--- front-end/app/src/components/Header.jsx
+++ front-end/app/src/components/Header.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../image/logo.png';
+import menu from '../image/menu.png';
+import '../styles/Header.css';
+import SearchContext from '../context/SearchContext';
 
 function Header() {
+  const { isActive, setIsActive } = useContext(SearchContext);
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <header className="header">
-      <img src={ logo } alt="logo internet segura" width="100px" />
-      <nav>
-        <Link to="/inicio">Início</Link>
-        <Link to="/pesquisa">Aprenda</Link>
-        <Link to="/sobre">Sobre</Link>
-      </nav>
+      <div className="headerContainer">
+        <img src={ logo } alt="logo internet segura" width="100px" />
+        <nav className="navbar">
+          <Link to="/inicio">Início</Link>
+          <Link to="/pesquisa">Aprenda</Link>
+          <Link to="/sobre">Sobre</Link>
+        </nav>
+        <button
+          type="button"
+          className="menuNavBar"
+          onClick={ handleClick }
+        >
+          <img src={ menu } alt="menu from icon8" />
+        </button>
+      </div>
     </header>
   );
 }
