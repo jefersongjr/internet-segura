3cf6e90771e36ea644d2f892c84bf695796fdcdf
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-09-30 17:47:49 -0300

    feat :sparkles: criando página Pesquisa

--- front-end/src/pages/Search.jsx
+++ front-end/src/pages/Search.jsx
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SearchContext from '../context/SearchContext';
+import crianca from "../image/criançaPesquisa.jpeg"
+import adolescente from "../image/adolescentePesquisa.jpg"
+import '../styles/Search.css';
 
 function Search() {
   const { data, setSearch } = useContext(SearchContext);
@@ -15,18 +18,40 @@ function Search() {
 
   return (
     <div className="Search">
-      <button
+      <section className='container'>
+        <div className='button-section'>
+          <img src={crianca} width="100%" alt="criança mexendo em tablet" />
+        </div>
+        <div className='button-container'>
+          <p> Principais recomendações</p>
+          <p>para crianças entre </p>
+          <p>2 e 10 anos.</p>
+        <button
+        className='button-search childButton'
         type="button"
         onClick={ (e) => handleClick(e) }
       >
         Criança
       </button>
-      <button
+        </div>
+      </section>
+      <section className='container'>
+        <div className='button-container'>
+          <p> Principais recomendações</p>
+          <p>para crianças entre </p>
+          <p> 11 e 17 anos.</p>
+        <button
+        className='button-search teenButton'
         type="button"
         onClick={ (e) => handleClick(e) }
       >
         Adolescente
       </button>
+        </div>
+        <div className='button-section'>
+          <img src={adolescente} width="100%" alt="criança mexendo em tablet" />
+        </div>
+      </section>
     </div>
   );
 }
