1970553dfbf0473dd03a16c554325de6aa13f8e9
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-09-30 17:56:43 -0300

    fix :bug: corrigindo permissão de cors

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -5,7 +5,7 @@ const DataRoute = require('./routes/DataRoute');
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://internet-segura.vercel.app',
     optionsSuccessStatus: 200
   }
 
